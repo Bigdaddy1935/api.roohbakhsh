3ccf5630af0d48f8c75b07f131763e96ad84317a
Bigdaddy1935 <emad.gh1935@gmail.com>
2022-12-11 12:27:55 +0330

    add product by id json to lessons search method

--- app/Repositories/SearchRepository.php
+++ app/Repositories/SearchRepository.php
@@ -30,7 +30,18 @@ class SearchRepository  implements SearchRepositoryInterface
         with('courses',function ($q) use ($user){
             $q->join("users","users.id",'=',"courses.course_user_id")
                 ->select("courses.*","users.fullname")
-                ->with('products')
+                ->with('products',function ($q) use ($user){
+                    $q->withExists(['bookmarkableBookmarks as bookmark'=>function($q) use ($user){
+                        $q->where('user_id',$user);
+                    }])
+                        ->with('categories')
+                        ->with(['courses'=>function ($q) {
+                            $q->join('users','users.id','=','courses.course_user_id')
+                                ->select('courses.*','users.fullname')->withCount('lessons');
+                        }])->with(['related'=>function ( $q){
+                            $q->with('courses');
+                        }]) ->orderBy('id','DESC')->get()->toArray();
+                })
                 ->with("categories")
                 ->withCount("lessons")
                 ->withExists(['bookmarkableBookmarks as bookmark'=>function($q) use ($user){
@@ -82,29 +93,31 @@ class SearchRepository  implements SearchRepositoryInterface
 
     public function SearchInLesson($req, $user)
     {
-     return   Lesson::query()->where('title','LIKE',"%{$req}%")->
-            with('courses',function ($q) use ($user){
-                $q->join("users","users.id",'=',"courses.course_user_id")
-                    ->select("courses.*","users.fullname")
-                    ->with('products', function ($q) use ($user){
-                        $q->wherehas('courses',function (Builder $q) use ($req) {
-                            $q->where('course_title','LIKE',"%{$req}%");
-                        })->withExists(['bookmarkableBookmarks as bookmark'=>function($q) use ($user){
-                            $q->where('user_id',$user);
-                        }])
-                            ->with('categories')
-                            ->with(['courses'=>function ($q) {
-                                $q->withCount('lessons');
-                            }])->join('courses','courses.id','products.course_id')->orderBy('id','DESC')->get()->toArray();
-                    })
-                    ->with("categories")
-                    ->withCount("lessons")
-                    ->withExists(['bookmarkableBookmarks as bookmark'=>function($q) use ($user){
+        return   Lesson::query()->
+        with('courses',function ($q) use ($user){
+            $q->join("users","users.id",'=',"courses.course_user_id")
+                ->select("courses.*","users.fullname")
+                ->with('products',function ($q) use ($user){
+                    $q->withExists(['bookmarkableBookmarks as bookmark'=>function($q) use ($user){
                         $q->where('user_id',$user);
-                    }])->orderBy('id','DESC')->get()->toArray();
-            })
+                    }])
+                        ->with('categories')
+                        ->with(['courses'=>function ($q) {
+                            $q->join('users','users.id','=','courses.course_user_id')
+                                ->select('courses.*','users.fullname')->withCount('lessons');
+                        }])->with(['related'=>function ( $q){
+                            $q->with('courses');
+                        }]) ->orderBy('id','DESC')->get()->toArray();
+                })
+                ->with("categories")
+                ->withCount("lessons")
+                ->withExists(['bookmarkableBookmarks as bookmark'=>function($q) use ($user){
+                    $q->where('user_id',$user);
+                }])->orderBy('id','DESC')->get()->toArray();
+        })->where('title','LIKE',"%{$req}%")
             ->orderBy('id','DESC')->get()->toArray();
     }
+
 
     public function SearchInProductFirstWord($req, $user)
     {
