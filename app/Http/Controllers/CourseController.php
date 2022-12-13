828950923b850b5e8e134ea7abf1dcf29d95fa25
Bigdaddy1935 <emad.gh1935@gmail.com>
2022-12-13 15:32:11 +0330

    fix course progress

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -73,12 +73,12 @@ protected $result=[];
      $course=$this->courseRepository->GetCoursesData();
 
         for ($i=0;$i<count($course['data']);$i++){
-            $totalProgress = 0;
+          $totalProgress=0.0;
             $newRes = $course['data'][$i]['lessons'];
             for($j=0;$j < count($newRes); $j++){
-                $totalProgress += doubleval($newRes[$j]['progress']['percentage']);
+                $totalProgress += $newRes[$j]['progress']['percentage'];
             }
-            $course['data'][$i]['courseProgress']=$totalProgress/($course['data'][$i]['lessons_count']);
+         $course['data'][$i]['courseProgress']=$totalProgress/$course['data'][$i]['lessons_count'];
         }
 
        if($course){
