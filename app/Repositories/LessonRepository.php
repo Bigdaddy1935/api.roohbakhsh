cf4c91ec495f6f5afb504e4766a85176c0d561f2
Bigdaddy1935 <emad.gh1935@gmail.com>
2023-01-01 13:30:44 +0330

    add get lessons of an media method..

--- app/Repositories/LessonRepository.php
+++ app/Repositories/LessonRepository.php
@@ -83,7 +83,7 @@ class LessonRepository extends Repository implements LessonRepositoryInterface
             }])
             ->with('progress',function ($q)use ($user){
                 $q->where('user_id',$user);
-            })->orderBy('id','ASC')
+            })
             ->paginate(10);
     }
 }
