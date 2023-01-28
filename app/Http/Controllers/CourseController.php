6231b71b68e2be7d1ffa7a76202616c13efd2c97
unknown <emad.gh1935@gmail.com>
2023-01-28 18:09:06 +0330

    fix percentage in course by id

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -123,7 +123,7 @@ protected $result=[];
         for($i = 0 ; $i < count($course['lessons']); $i++ ){
            $progressTotal +=  $course['lessons'][$i]['progress'][$i]['percentage'];
         }
-        $progressTotal = $progressTotal==0?0:  ($progressTotal / $course['lessons_count']);
+        $progressTotal = $progressTotal==0?0: sprintf("%.2f",($progressTotal / $course['lessons_count']));
         $course['courseProgress'] = $progressTotal;
 
         return response()->json([
