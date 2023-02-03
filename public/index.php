f771d84978a0a6f83f05797bd5926984b2edfac9
unknown <emad.gh1935@gmail.com>
2023-02-03 15:22:20 +0330

    api roohbakhshac v1 release

--- public/index.php
+++ public/index.php
@@ -31,7 +31,7 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 |
 */
 
-require __DIR__.'/../Roohbakhsh/vendor/autoload.php';
+require __DIR__.'/../vendor/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -44,7 +44,7 @@ require __DIR__.'/../Roohbakhsh/vendor/autoload.php';
 |
 */
 
-$app = require_once __DIR__.'/../Roohbakhsh/bootstrap/app.php';
+$app = require_once __DIR__.'/../bootstrap/app.php';
 
 $kernel = $app->make(Kernel::class);
 
