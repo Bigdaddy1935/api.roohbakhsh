176417314228f42d6a2071fbde0084d28c4ebbd7
unknown <emad.gh1935@gmail.com>
2023-02-21 14:30:46 +0330

    perv commit again

--- app/Repositories/LessonRepository.php
+++ app/Repositories/LessonRepository.php
@@ -141,7 +141,7 @@ class LessonRepository extends Repository implements LessonRepositoryInterface
             }])
             ->with(['progress'=>function ($q)use ($user){
                 $q->where('user_id',$user);
-            }])->with('related')
+            }])
             ->get();
     }
 }
